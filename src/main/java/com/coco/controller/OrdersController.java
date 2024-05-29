2b0e5c078476689c12ab999d2498d524fc538023
PHAN DUC <ducpa2002@gmail.com>
2024-05-29 23:02:15 +0700

    update

--- src/main/java/com/coco/controller/OrdersController.java
+++ src/main/java/com/coco/controller/OrdersController.java
@@ -4,20 +4,14 @@ import com.coco.dto.OrderFilterDTO;
 import com.coco.dto.OrderReqDTO;
 import com.coco.dto.OrderStatusDTO;
 import com.coco.dto.OrdersDTO;
-<<<<<<< HEAD
 import com.coco.entity.OrdersEntity;
 import com.coco.exception.DataNotFoundException;
-import com.coco.service.IOrdersService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-=======
 import com.coco.enumDefine.StatusOrderEnum;
 import com.coco.service.IOrdersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
->>>>>>> main
 
 @RestController
 @RequestMapping(value = "api/orders")
@@ -30,21 +24,20 @@ public class OrdersController {
     public OrdersDTO postOrders(@RequestBody OrderReqDTO ordReq){
         return ordersService.orderPaintings(ordReq);
     }
-<<<<<<< HEAD
+
     @PutMapping
     public OrdersEntity updateOrder(@RequestParam Long id, @RequestBody OrdersDTO ordersDTO) throws DataNotFoundException {
         return ordersService.updateOrder(id, ordersDTO);
-=======
+    }
 
     @GetMapping
     public List<OrdersDTO> getByCondition(@ModelAttribute OrderFilterDTO orderFilterDTO){
-
         return ordersService.getByCondition(orderFilterDTO);
     }
+
     @PutMapping("{id}")
     public OrdersDTO updateStatus(@RequestBody OrderStatusDTO orderStatusDTO, @PathVariable(value = "id") Long id){
         StatusOrderEnum statusEnum = StatusOrderEnum.fromString(orderStatusDTO.getStatus());
-        return ordersService.updateStatus(statusEnum.getValue(),id);
->>>>>>> main
+        return ordersService.updateStatus(statusEnum.getValue(), id);
     }
 }
